40595beb0a03aaa3d081fe670943820082bac1a7
soyto <alf.ixos@gmail.com>
2017-11-02 15:37:12 +0100

    [DEV] config refactor

--- node_app/config.js
+++ node_app/config.js
@@ -2,23 +2,27 @@
 module.exports = (function() {
   'use strict';
 
-  let _result = {
-    'server': {
-      'port': 80
-    },
-    'twitch': {
-      'clientID': 'eplbp72ytgf9g6f1h7td2q1fm1c6dl',
-      'redirectURI': 'http://localhost/twitch-login/'
-    },
-    'twitter': {
-      'CONSUMER_KEY': 'pEB31grzFFwxotNME9FlZqFRF',
-      'CONSUMER_SECRET': 'CGWGvVATToP8zFxH9p1nQH4ySB4aH8aVeGRIR9VGvHpre46QHy'
-    },
-    'debug': {
-    },
-    'persist': {
-      'pretty_print': false
-    }
+  let _result = {};
+
+  _result['server'] = {
+    'port': 80
+  };
+
+  _result['twitch'] = {
+    'clientID': 'eplbp72ytgf9g6f1h7td2q1fm1c6dl',
+    'redirectURI': 'http://localhost/twitch-login/'
+  };
+
+  _result['twitter'] = {
+    'CONSUMER_KEY': 'pEB31grzFFwxotNME9FlZqFRF',
+    'CONSUMER_SECRET': 'CGWGvVATToP8zFxH9p1nQH4ySB4aH8aVeGRIR9VGvHpre46QHy'
+  };
+
+  _result['debug'] = {
+  };
+
+  _result['persist'] = {
+    'pretty_print': true
   };
 
 
